6cd5cbd5211d603f7a3fbdad48a9f4b6bcc6efb5
Kushagra Varshney <devkussu43@gmail.com>
2024-04-25 00:24:53 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,310 @@
+let islightmode = false;
+let brightness_button = document.getElementById("brightness-btn");
+let body = document.getElementById("body");
+let header = document.querySelector(".header");
+let search_bar = document.getElementById("search-bar");
+let below_header_navbar = document.querySelector(".navbar");
+let navbarLinks = document.querySelectorAll(".navbar-nav .nav-link");
+let icon = brightness_button.querySelector("i");
+let company_logo = document.querySelector(".company-logo .image");
+
+let bank_offer = document.getElementById("bank-offer");
+let best_seller = document.getElementById("best-seller");
+let my_smart_basket = document.getElementById("my-smart-basket")
+
+let section_4 = document.querySelector("#section4");
+let section_5 = document.querySelector("#section5");
+let section_6 = document.querySelector("#section6");
+let section_7 = document.querySelector("#section7");
+let section_8 = document.querySelector("#section8");
+
+document.addEventListener("DOMContentLoaded", function() {
+    brightness_button.addEventListener("click", () => {
+        if (!islightmode) {
+            // Change to light mode
+            body.style.backgroundColor = "#FFFFFF";
+            // body.style.color = "#000000";
+            header.style.backgroundColor = "#34495e";
+            header.style.color = "#000000";
+            search_bar.style.backgroundColor = "#F8F9FA";
+            search_bar.style.color = "#000000";
+            below_header_navbar.style.backgroundColor = "#FFFFFF";
+            below_header_navbar.style.color = "#000000";
+            navbarLinks.forEach(link => {
+                link.style.color = "#000000";
+            });
+            // Add more styling changes here for light mode
+            icon.classList.remove("fa-sun");
+            icon.classList.add("fa-moon");
+            company_logo.style.backgroundColor = "#FFFFFF";
+
+            bank_offer.style.color = "#000000";
+
+
+            section_4.style.backgroundColor = "#34495e";
+            section_5.style.backgroundColor = "#F7F7F7";
+            section_6.style.backgroundColor = "#34495e";
+            section_7.style.backgroundColor = "#F7F7F7";
+            section_8.style.backgroundColor = "#F7F7F7";
+            section_8.style.color = "#000000";
+
+            
+
+            islightmode = true;
+        } else {
+            // Change to dark mode
+            body.style.backgroundColor = "#131417";
+            // body.style.color = "#FFFFFF";
+
+            header.style.backgroundColor = "#222221";
+            header.style.color = "#FFFFFF";
+
+            search_bar.style.backgroundColor = "#3c3c3c";
+            search_bar.style.color = "#FFFFFF";
+
+            below_header_navbar.style.setProperty('background-color', '#222221', 'important');
+            below_header_navbar.style.color = "#FFFFFF";
+
+            navbarLinks.forEach(link => {
+                link.style.color = "#FFFFFF";
+            });
+            // Add more styling changes here for dark mode
+            icon.classList.remove("fa-moon");
+            icon.classList.add("fa-sun");
+            company_logo.style.backgroundColor = "#FFFFFF";
+
+            best_seller.style.color = "#FFFFFF";
+            my_smart_basket.style.color = "#FFFFFF";
+            bank_offer.style.color = "#FFFFFF";
+
+            section_4.style.backgroundColor = "#222221";
+            section_5.style.backgroundColor = "#222221";
+            section_6.style.backgroundColor = "#222221";
+            section_7.style.backgroundColor = "#222221";
+            section_8.style.backgroundColor = "#222221";
+            section_8.style.color = "#FFFFFF";
+
+            islightmode = false;
+        }
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    let navLinks = document.querySelectorAll(".navbar-nav .nav-link");
+    navLinks.forEach(function(link) {
+        link.addEventListener("mouseenter", function() {
+            this.classList.add("hovered");
+        });
+        link.addEventListener("mouseleave", function() {
+            this.classList.remove("hovered");
+        });
+    });
+});
+
+
+
+
+
+// - - - - - - - - - - - - - - - - - - - - - - -code for search bar - - - - - - - - - - - - - - - - - - - - - - -  - -
+
+// as i already have search_bar variable above so use it
+search_bar.addEventListener("keydown" , function(event) {
+    // Check if the pressed key is 'Enter' (key code 13)
+    if(event.keyCode === 13) {
+        // Get the value entered by the user
+        const searchTerm = search_bar.value.toLowerCase().trim();
+
+        // Redirect the user to the corresponding HTML page based on the search term
+        switch (searchTerm) {
+            case 'home':
+                window.location.href = 'index.html';
+                break;
+            case 'tea':
+                window.location.href = 'tea.html';
+                break;
+            // Add cases for other search terms and corresponding HTML pages
+            case 'bakery':
+                window.location.href = 'bakery.html';
+                break;
+            case 'kirana':
+                    window.location.href = 'kirana.html';
+                break;
+            case 'refined':
+                    window.location.href = 'refined_oil.html';
+                break;
+            case 'oil':
+                    window.location.href = 'refined_oil.html';
+                break;
+            case 'fruits':
+                    window.location.href = 'exotic_fruits_vegetables.html';
+                break;
+            case 'vegetables':
+                    window.location.href = 'exotic_fruits_vegetables.html';
+                break;
+            default:
+                // If no matching page is found, do nothing or display an error message
+                alert('No matching page found for the search term.');
+                console.log('No matching page found for the search term.');
+                break;
+        }
+    }
+});
+
+
+
+// - - - - - - - - - - - - - - - - - - - - - - -code for search bar - - - - - - - - - - - - - - - - - - - - - - -  - -
+
+
+
+
+
+
+// - - - - - - - - - - - - - - - - - - - - - - -code for Cart - - - - - - - - - - - - - - - - - - - - - - -  - -
+
+
+
+let cart = [];
+
+
+// Function to initialize the cart from local storage
+function initializeCart() {
+    // Retrieve cart data from local storage
+    const storedCart = localStorage.getItem('cart');
+    // Parse the stored cart data or initialize an empty array if no data is found
+    cart = storedCart ? JSON.parse(storedCart) : [];
+}
+
+// Initialize the cart when the page loads
+initializeCart();
+
+// Function to update the cart in local storage
+function updateLocalStorage() {
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+// Assume you have an "Add" button for each item with a class "add-to-cart-button"
+let addItemButtons = document.querySelectorAll('.add-to-cart-button');
+addItemButtons.forEach(button => {
+    button.addEventListener('click', function() {
+        let itemId = button.dataset.id;
+        let itemName = button.dataset.name;
+        let itemPrice = parseFloat(button.dataset.price);
+
+        
+        // Add the item to the cart array
+        let item = { id: itemId, name: itemName, price: itemPrice, quantity: 1 };
+        addToCart(item);
+
+        // Show alert message when an item is added to the cart
+        alert(`Item with ID ${itemId} and name ${itemName} is added to the cart.`);
+
+        // Log cart contents to the console
+        console.log("Cart contents:", cart);
+
+        // Update the cart in local storage
+        updateLocalStorage();
+        // let total_amount = calculateTotalPayment(cart);
+        // console.log(total_amount);
+        // document.getElementById("payment-total-amount").innerText = "";
+        // document.getElementById("payment-total-amount").innerText = total_amount;
+    });
+});
+
+
+
+
+// Function to add an item to the cart
+function addToCart(item) {
+    // Check if the item is already in the cart
+    let existingItem = cart.find(cartItem => cartItem.id === item.id);
+    if (existingItem) {
+        // If the item is already in the cart, update the quantity
+        existingItem.quantity += 1;
+    } else {
+        // If the item is not in the cart, add it
+        cart.push(item);
+    }
+
+}
+
+
+
+
+
+document.getElementById('clear-cart-button').addEventListener('click', function() {
+    // Call the clearCart function when the button is clicked
+    clearCart();
+});
+
+
+// Function to clear the cart
+function clearCart() {
+    // Clear the cart array
+    cart = [];
+
+    // Update the cart display after clearing the cart
+    // updateCart();
+
+    // Also, update the cart data in local storage
+    updateLocalStorage();
+
+}
+
+
+// Function to calculate the total payment
+// function calculateTotalPayment(cart) {
+//     let total = 0;
+//     for (let item of cart) {
+//         total += item.price * item.quantity;
+//     }
+//     return total.toFixed(2); // Return the total payment rounded to 2 decimal places
+// }
+document.getElementById("place-order-now").addEventListener("click", () => {
+    console.log("clicked")
+})
+
+document.getElementById('place-order-now').addEventListener('click' , () => {
+    console.log("in place order")
+    let total = 0.0;
+    console.log("in place order")
+    alert("hi")
+    for(let item of cart) {
+        total += item.price * item.quantity;
+        console.log(item.price);
+    }
+    const pay_amount = document.getElementById('pay-total-amount');
+    console.log("total " + total);
+    pay_amount.innerHTML = "";
+    pay_amount.innerHTML = total;
+})
+
+
+// document.getElementById('go-to-payment-page').addEventListener('click', function() {
+//     // Calculate total payment
+//     let totalPayment = calculateTotalPayment();
+
+//     // Check if totalPayment is a number
+//     if (typeof totalPayment === 'number') {
+//         // Store total payment value in localStorage
+//         localStorage.setItem('totalPayment', totalPayment.toFixed(2));
+
+//         // Redirect to payment.html
+//         window.location.href = 'payment.html';
+//     } else {
+//         console.error('Total payment is not a valid number.');
+//     }
+// });
+// - - - - - - - - - - - - - - - - - - - - - - -code for Cart - - - - - - - - - - - - - - - - - - - - - - -  - -
+
+
+
+
+
+
+
+
+// - - - - - - - - - - - - - - - - - - - - - - -code for Contact us page - - - - - - - - - - - - - - - - - - - - - - -  - -
+
+
+
+// - - - - - - - - - - - - - - - - - - - - - - -code for Contact us page - - - - - - - - - - - - - - - - - - - - - - -  - -
---
+++ server.js
@@ -0,0 +1,250 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyparser = require("body-parser");
+const session = require('express-session');
+const path = require('path');
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+app.use(session({
+    secret: 'my-secret-string',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false, maxAge: 60000 } // Set the session cookie to expire after 1 minute for testing purposes
+  }));
+  
+app.use((req, res, next) => {
+    if (req.url.endsWith('.js')) {
+        res.setHeader('Content-Type', 'application/javascript');
+    }
+    next();
+});
+
+app.use(bodyparser.urlencoded({extended : false}))
+
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI);
+
+// Middleware for parsing JSON bodies
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve the script.js file from the grocery directory
+app.get('/script.js', (req, res) => {
+    res.sendFile(path.join(__dirname, 'script.js'));
+});
+
+// Define user schema and model for login/sign page
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String
+});
+
+const User = mongoose.model("login_users", userSchema);
+
+
+// Define user schema and model for contact page
+const contactUserSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    subject:String,
+    message:String
+  });
+  
+const contactUser = mongoose.model("contact_users", contactUserSchema);
+
+
+
+const paymentUserSchema = new mongoose.Schema({
+    fname: String,
+    lname: String,
+    address: String,
+    country:String,
+    city:String,
+    pAmount:Number
+  });
+  
+const paymentUser = mongoose.model("payment_users", paymentUserSchema);
+
+
+
+
+
+// Route to render the index page
+app.get("/index", (req, res) => {
+  res.sendFile(__dirname + "/public/" + "index.html");
+});
+
+app.get("/exotic_fruits_vegetables", (req, res) => {
+    res.sendFile(__dirname + "/public/" + "exotic_fruits_vegetables.html");
+});
+
+app.get("/tea", (req, res) => {
+    res.sendFile(__dirname + "/public/" + "tea.html");
+});
+
+app.get("/refined_oil", (req, res) => {
+    res.sendFile(__dirname + "/public/" + "refined_oil.html");
+});
+
+app.get("/kirana", (req, res) => {
+    res.sendFile(__dirname + "/public/" + "kirana.html");
+});
+
+app.get("/contact", (req, res) => {
+    res.sendFile(__dirname + "/public/" + "contact.html");
+});
+
+app.get("/payment", (req, res) => {
+    res.sendFile(__dirname + "/public/" + "payment.html");
+});
+
+app.get("/bakery", (req, res) => {
+    res.sendFile(__dirname + "/public/" + "bakery.html");
+});
+
+
+app.get("/login" , (req, res) => {
+    res.sendFile(__dirname + "/public/" + "index.html");
+})
+
+app.post("/login" , (req , res) => {
+    const email = req.body.user_email;
+    const pass = req.body.user_password;
+    // Check if the provided email exists in the database
+    User.findOne({ email: email })
+        .then(foundUser => {
+            if (foundUser) {
+                // If email exists, compare the passwords
+                if (foundUser.password === pass) {
+                    // Passwords match, set session variable to indicate that the user is logged in
+                    console.log("Successfully logged in");
+                    res.send(`<script>alert('Successfully logged in'); window.location.href = '/index';</script>`);
+                    req.session.loggedIn = true;
+                    req.session.save();
+                    // res.redirect("/index"); // Redirect to the index page if login is successful
+                } else {
+                    res.send(`<script>alert('Invalid email or password. Please try again.'); window.location.href = '/login';</script>`);
+                }
+            } else {
+                res.send(`<script>alert('Email not found. Please sign up first.'); window.location.href = '/sign';</script>`);
+                app.get("/sign" , (req , res) => {
+                    res.sendFile(__dirname + "/public/" + "sign.html");
+                })
+
+                app.post("/sign" , (req , res) => {
+                    const u_name = req.body.user_sign_name;
+                    const u_email = req.body.user_sign_email;
+                    const u_pass = req.body.user_sign_password;
+                
+                    // Check if the provided email already exists in the database
+                    User.findOne({email: u_email})
+                        .then(foundUser => {
+                            if(!foundUser) {
+                                // Email not found, create a new user
+                                const data = new User({name: u_name, email: u_email, password: u_pass});
+                                data.save()
+                                    .then(doc => {
+                                        console.log(doc);
+                                        req.session.loggedIn = true;
+                                        req.session.save();
+                                        console.log("Successfully signed in")
+                                        res.send(`<script>alert('Successfully signed in'); window.location.href = '/index';</script>`);
+                                        // res.redirect("/index"); // Redirect to the index page if sign-up is successful
+                                    })
+                                    .catch(err => {
+                                        console.log("An error has occured in the app.post");
+                                    });
+                            } else {
+                                // Email already exists, show error message and redirect to sign-up page
+                                res.send(`<script>alert('Email is already taken. Please use a different email to sign up.'); window.location.href = '/sign';</script>`);
+                            }
+                        });
+                });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.send("Error occurred during login.");
+        });
+});
+
+
+// Add this route to check login status
+app.get("/login-status", (req, res) => {
+    if (req.session.loggedIn) {
+      res.json({ loggedIn: true });
+    } else {
+      res.json({ loggedIn: false });
+    }
+  });
+
+
+
+
+
+app.post("/contact", (req, res) => {
+    const c_name = req.body.contact_user_name;
+    const c_email = req.body.contact_email;
+    const c_subject = req.body.contact_subject;
+    const c_message = req.body.contact_message;
+
+    // Check if any field is empty
+    if (c_name === "" || c_email === "" || c_subject === "" || c_message === "") {
+        return res.send(`<script>alert('Some of the fields has not been filled by you! Please fill these first to send the message.'); window.location.href = '/contact';</script>`);
+    }
+
+    // If all fields are filled, proceed to save data
+    const data = new contactUser({ name: c_name, email: c_email, subject: c_subject, message: c_message });
+    data.save()
+        .then(doc => {
+            console.log(doc);
+            console.log("Successfully sent the message and saved into database");
+            res.send(`<script>alert('Successfully sent the message'); window.location.href = '/index';</script>`);
+        })
+        .catch(err => {
+            console.log("An error has occurred in the app.post");
+            // Handle the error appropriately, e.g., by sending an error response
+            res.status(500).send("An error occurred while processing your request.");
+        });
+});
+
+
+
+
+app.post("/payment", (req, res) => {
+    const p_fname = req.body.payment_first_name;
+    const p_lname = req.body.payment_last_name;
+    const p_address = req.body.payment_adress;
+    const p_country = req.body.payment_country;
+    const p_city = req.body.payment_city;
+    const p_amount = req.body.payment_amount;
+
+    // Check if any field is empty
+    if (p_fname === "" || p_lname === "" || p_address === "" || p_country === "" || p_city === "" ||p_amount === "") {
+        return res.send(`<script>alert('Some of the fields has not been filled by you! Please fill these first to send the message.'); window.location.href = '/payment';</script>`);
+    }
+
+    // If all fields are filled, proceed to save data
+    const data = new paymentUser({ fname: p_fname, lname: p_lname, address: p_address, country: p_country, city: p_city, pAmount: p_amount});
+    data.save()
+        .then(doc => {
+            console.log(doc);
+            console.log("Successfully sent the message and saved into database");
+            res.send(`<script>alert('Successfully done the payment'); window.location.href = '/index';</script>`);
+        })
+        .catch(err => {
+            console.log("An error has occurred in the app.post");
+            // Handle the error appropriately, e.g., by sending an error response
+            res.status(500).send("An error occurred while processing your request.");
+        });
+});
+
+
+
+app.listen(2004 , function(err) {
+    console.log("server is live");
+})
